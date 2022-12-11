1a4c5cfffd4b5228b1ee58ec82736e297dec5a8d
bopboy <39391891+bopboy@users.noreply.github.com>
2022-12-11 13:13:14 +0900

    컴포넌트 스타일 설정 방법

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const isError = true;
   return (
     <View style={styles.container}>
-      <Text>RN Calculator App</Text>
+      <Text style={styles.text}>RN Calculator App</Text>
+      <Text style={styles.text}>RN Calculator App2</Text>
+      <Text style={[styles.text, styles.error]}>Error Message</Text>
+      <Text style={[styles.error, styles.text]}>Error Message</Text>
+      <Text style={[styles.text, isError && styles.error]}>Error Message</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +21,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "700",
+    color: "green",
+  },
+  error: {
+    color: "red",
   },
 });
